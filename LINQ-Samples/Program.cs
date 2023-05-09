52571cccd6d1f52260d6319937c28742af1fe141
Allan Prieto <allanbanilla@gmail.com>
2023-05-09 00:22:53 -0600

    Agregar archivos de proyecto.

---
+++ LINQ-Samples/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LINQ_Samples
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            // Llamar al método Main de la clase LINQ_query_structure
+            LINQ_Samples.YourFirstQuery.LINQ_query_structure.Ejemplo();
+
+        }
+    }
+}
---
+++ LINQ-Samples/YourFirstQuery/LINQ_query_structure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Samples.YourFirstQuery
+{
+    public class LINQ_query_structure
+    {
+        public static void Ejemplo()
+        {
+            // LINQ query structure
+            // This sample uses where to find all elements of an array less than 5. 
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var lowNums = from num in numbers
+                          where num < 5
+                          select num;
+
+            Console.WriteLine("Number < 5:");
+            foreach (var x in lowNums)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
