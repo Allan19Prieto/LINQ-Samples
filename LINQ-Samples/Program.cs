d41435a12c1a52bcbe15a935797b79d67f58ce4a
Allan Prieto <allanbanilla@gmail.com>
2023-05-09 00:45:40 -0600

    Nuevo Ejemplo

--- LINQ-Samples/Program.cs
+++ LINQ-Samples/Program.cs
@@ -9,6 +9,7 @@ namespace LINQ_Samples
 
             // Llamar al método Main de la clase LINQ_query_structure
             LINQ_Samples.YourFirstQuery.LINQ_query_structure.Ejemplo();
+            LINQ_Samples.YourFirstQuery.Filter_Elements_on_a_property.Ejemplo();
 
         }
     }
---
+++ LINQ-Samples/YourFirstQuery/Filter_Elements_on_a_property.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Samples.YourFirstQuery
+{
+    internal class Filter_Elements_on_a_property
+    {
+        public static void Ejemplo()
+        {
+            
+            List<Product> products = GetProductList();
+
+            var soldOutProducts = from prod in products
+                                  where prod.UnitsInStock == 0
+                                  select prod;
+            Console.WriteLine("Sold out products:");
+            foreach ( var produt in soldOutProducts)
+            {
+                Console.WriteLine($"{produt.ProductName} is sold out!");
+            }
+        }
+
+        public class Product
+        {
+            public int ProductID { get; set; }
+            public string ProductName { get; set; }
+            public int UnitsInStock { get; set; }
+        }
+
+        public static List<Product> GetProductList()
+        {
+            return new List<Product>
+            {
+                new Product { ProductID = 1, ProductName = "Chai", UnitsInStock = 10 },
+                new Product { ProductID = 2, ProductName = "Chang", UnitsInStock = 0 },
+                new Product { ProductID = 3, ProductName = "Aniseed Syrup", UnitsInStock = 20 },
+                new Product { ProductID = 4, ProductName = "Chef Anton's Cajun Seasoning", UnitsInStock = 0 },
+                new Product { ProductID = 5, ProductName = "Chef Anton's Gumbo Mix", UnitsInStock = 0 },
+                // agregar más productos aquí
+            };
+        }
+
+    }
+}
